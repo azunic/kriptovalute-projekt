d1d3f8017875d49bc5b2fd2cd0779322d2293495
Ana Žunić <zunicana13@gmail.com>
2020-01-26 21:00:57 +0100

    spajanje na bitcoin core apii

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const morgan =  require("morgan");
+const bitcoinRpc = require("node-bitcoin-rpc");
 const port = 8080;
 
 const app = express();
 app.use(morgan("dev"));
 
 app.get("/projekt", (req, res) => {
-    res.send("Hello World");
+
+    try{
+        bitcoinRpc.init('blockchain.oss.unist.hr', '8332', 'student', 'WYVyF5DTERJASAiIiYGg4UkRH');
+        bitcoinRpc.call('getblockcount', [], function (err, btc_res) {
+            if (err) {
+              const errMsg = "Dogodila se greska prilikom pozivanja Bitcoin Core Api-ija: " + err;
+              console.log(errMsg);
+              throw new Error(errMsg);
+            } 
+            else if (btc_res.error) {
+              const errMsg = "Pogreska primljena od strane Bitcoin Core-a " 
+              + btc_res.error.message 
+              + " (" + btc_res.error.code + ")";
+
+              console.log(errMsg);
+              throw new Error(errMsg);
+            } 
+            else {
+              console.log('Odgovor od Bitcoin Core api-ija: ')
+              console.log(btc_res.result);
+              res.send("Get block count: " + btc_res.result);
+            }
+        });
+
+    }catch(err){
+        console.error("Dogodila se pogreska",err);
+    }
+    
 });
 
 app.listen(port, () => {
